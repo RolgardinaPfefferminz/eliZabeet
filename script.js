4941a7427815659a80d7b59f881e42cedc7173c1
Sarah <128378817+RolgardinaPfefferminz@users.noreply.github.com>
2026-09-12 16:31:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,12 +47,14 @@ function renderMap(){
     const visited = routeOrder.includes(st.id);
     pin.className = "station-pin" + (st.isStart ? " start" : "") + (visited ? " visited" : "") + (!visited && !st.isStart ? " pulsing" : "");
     pin.style.left = st.pos.x + "%";
-   
+
      if(st.isStart){
   pin.innerHTML = "📍";
   pin.classList.add("preview-start");
-  pin.style.left = "72.3%";
+  pin.style.left = "78%";
+  pin.style.top = "35%";
 }
+  
     pin.style.top = st.pos.y + "%";
    
     pin.onclick = () => openPanel(st.id);
